bfcfbf659889d16ad23c846789798e65b11203f2
Roberto Ruffolo <robertoruffolo@hotmail.com>
2017-04-09 02:07:17 -0400

    Can now classify the persian handwritting test data, by using two method support vector classification and random forest classifier

--- PersianRecognition.py
+++ PersianRecognition.py
@@ -1,84 +1,81 @@
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
-import random
-from sklearn import ensemble
-import pandas as pd
+from sklearn import ensemble, svm, metrics
 import glob, os
 import numpy as np
 from PIL import Image
 import pylab as pl 
 
 def loadDigits(digit):
+	new_width = 64
+	new_height = 64
+	
 	list = []
 	for file in glob.glob("data/" + str(digit) + "/*"):
 		im = Image.open(file)
+		im = im.resize((new_width, new_height), Image.ANTIALIAS)
 		list.append(np.array(im))
 	return list
 
-
-
-
 def main():
-	digits = load_digits() 	
-	persianDigits = {'0' : [], '1' : [], '2' : [], '3' : [], '4' : [], '5' : [], '6' : [], '7' : [], '8' : [], '9' : []}
-	# for num in range(0,9):
-	# 	persianDigits[str(num)] = loadDigits(num)
-	# print(len(persianDigits['0']))
-	# print(digits.images[0])
-	# print("=========================")
-	# print(persianDigits['0'][0])
-	# print(persianDigits['0'][0])
-	
+	persianDigits = []
+	persianLabels = []
+	images_and_labels = []
+	for num in range(0,10):
+		digits = loadDigits(num)
+		persianDigits.extend(digits)
+		persianLabels.extend([num] * len(digits))
+
 	pl.gray() 
-	# pl.matshow(persianDigits['0'][0]) 
-	pl.matshow(digits.images[0]) 
+	pl.matshow(persianDigits[13]) 
 	pl.show() 	
 
-	digits.images[0] 	
-
-	print(digits.target)
-	images_and_labels = list(zip(digits.images, digits.target))
+	images_and_labels = list(zip(persianDigits, persianLabels))
 	for index, (image, label) in enumerate(images_and_labels[:15]):
 	    plt.subplot(3, 5, index + 1)
 	    plt.axis('off')
 	    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
 	    plt.title('%i' % label)
 
-	#Define variables
-	n_samples = len(digits.images)
-	x = digits.images.reshape((n_samples, -1))
-	y = digits.target
+	# To apply a classifier on this data, we need to flatten the image, to
+	# turn the data in a (samples, feature) matrix:
+	n_samples = len(persianDigits)
+	setDivision = int(n_samples/2)
+	from sklearn.utils import shuffle
+	persianDigits, persianLabels = shuffle(persianDigits, persianLabels, random_state=0)
+	data = np.array(persianDigits).reshape((n_samples, -1))
 
-	#Create random indices 
-	print(len(x))
-	sample_index=random.sample(range(len(x)),len(x)/5) #20-80
-	valid_index=[i for i in range(len(x)) if i not in sample_index]
+	# Create a classifier: a support vector classifier
+	classifier = svm.SVC(gamma=0.001)
 
-	#Sample and validation images
-	sample_images=[x[i] for i in sample_index]
-	valid_images=[x[i] for i in valid_index]
+	# We learn the digits on the first half of the digits
+	classifier.fit(data[:setDivision], persianLabels[:setDivision])
 
-	#Sample and validation targets
-	sample_target=[y[i] for i in sample_index]
-	valid_target=[y[i] for i in valid_index]
+	# Now predict the value of the digit on the second half:
+	expected = persianLabels[setDivision:]
+	predicted = classifier.predict(data[setDivision:])
 
-	#Using the Random Tree Classifier
+	print("Classification report for classifier %s:\n%s\n"
+	      % (classifier, metrics.classification_report(expected, predicted)))
+	print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+
+	# #Using the Random Tree Classifier
 	classifier = ensemble.RandomForestClassifier()
 
-	#Fit model with sample data
-	classifier.fit(sample_images, sample_target)
+	# #Fit model with sample data
+	classifier.fit(data[:setDivision], persianLabels[:setDivision])
 
-	#Attempt to predict validation data
-	score=classifier.score(valid_images, valid_target)
+	# #Attempt to predict validation data
+	score=classifier.score(data[setDivision:], expected)
 	print('Random Tree Classifier:\n') 
 	print('Score\t'+str(score))
 
+	images_and_predictions = list(zip(persianDigits[setDivision:], predicted))
+	for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+	    plt.subplot(2, 4, index + 5)
+	    plt.axis('off')
+	    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+	    plt.title('Prediction: %i' % prediction)
 
-	i=150
-
-	pl.gray() 
-	pl.matshow(digits.images[i]) 
-	pl.show() 
-	classifier.predict(x[i])
+	plt.show()
 
 main()
